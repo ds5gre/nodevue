8558247728c1423b96e75cdea5f4d53ca6a502a2
ds5gre <ds5gre@gmail.com>
2021-05-26 11:27:56 +0900

    add factorize

--- test/mysqltest2.js
+++ test/mysqltest2.js
@@ -1,3 +1,4 @@
+const { executionAsyncResource } = require('async_hooks');
 const { pathToFileURL } = require('url');
 const util  = require('util'),
       Promise = require('bluebird');
@@ -10,21 +11,46 @@ const sql2 = "select count(*) from time_zone";
 const pool = new Pool();
 
 Promise.using( pool.connect(), conn => {
-    Promise.all([
+    conn.beginTransaction( txerr => {
 
-        conn.queryAsync(sql1),
-        conn.queryAsync(sql2)
-    ]).then( r => {
-        console.log("End of Then !!!!!");
-        console.log("sql1=", r[0]);
-        console.log("sql2=", r[1]);
-        pool.end();
-
-    }).catch( err => {
-        console.log("Error:::::", err);
-        pool.end();
+        Promise.all([
+            conn.queryAsync(sql1),
+            conn.queryAsync(sql2)
+        ]).then (r => {
+            for (let i=0; i<r.length; i++)
+            console.log(`sql${i+1}=`, r[i]);
+            conn.commit();
+            pool.end();
+        }).catch( e => {
+            conn.rollback();
+            pool.end();
+        });
     });
 });
+
+function execut(fn){
+    Promise.using( pool.connect(), conn => {
+        conn.beginTransaction( txeer => {
+            fn(conn);
+        });
+    });
+}
+// Promise.using( pool.connect(), conn => {
+//     Promise.all([
+
+//         conn.queryAsync(sql1),
+//         conn.queryAsync(sql2)
+//     ]).then( r => {
+//         console.log("End of Then !!!!!");
+//         console.log("sql1=", r[0]);
+//         console.log("sql2=", r[1]);
+//         pool.end();
+
+//     }).catch( err => {
+//         console.log("Error:::::", err);
+//         pool.end();
+//     });
+// });
 
 // Promise.using( pool.connect(), conn =>{
 //     conn.queryAsync(sql1)
